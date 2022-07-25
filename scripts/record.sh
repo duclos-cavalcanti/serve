46db909d8d8347c790c99ed1dcdfae9bac394071
Daniel Duclos-Cavalcanti <daniel.duclos.cavalcanti@gmail.com>
2022-07-25 22:20:30 +0200

    Added recording script and also corresponding videos/gifs

---
+++ scripts/record.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+checkDependency() {
+    cmd=$1
+    if ! command -v ${cmd} &>/dev/null; then
+        echo "The following program is not found: ${cmd}"
+        exit 1
+    fi
+}
+
+checkDependency ffmpeg
+# checkDependency xwininfo
+
+# geometry=$(xwininfo | grep geometry | awk '{print $2}')
+
+ffmpeg -f x11grab -video_size 1920x1080 -framerate 50 -i :0.0+1920 -vf format=yuv420p ./.assets/example.mp4
+ffmpeg -ss 30 -t 3 -i ./.assets/example.mp4 -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 ./.assets/example.gif
+
